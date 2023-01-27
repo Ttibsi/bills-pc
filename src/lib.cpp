2a13855ec3f7830fe8997b17b76316fc9d8394c2
Ttibsi <ashisbitt@icloud.com>
2023-01-27 15:16:19 +0000

    update

--- src/lib.cpp
+++ src/lib.cpp
@@ -7,7 +7,7 @@ namespace py = pybind11;
 
 int add(int x, int y) { return x + y; }
 
-std::vector<std::string> get_from_db() {
+std::vector<std::string> get_from_db(std::string cmd) {
     std::vector<std::string> ret;
     sqlite3 *db;
     const auto res = sqlite3_open("./db.db", &db);
@@ -16,8 +16,21 @@ std::vector<std::string> get_from_db() {
         return ret;
     }
 
-    std::string stmt = "SELECT * FROM ABILITIES;";
-    // sqlite3_prepare_v2(db, )
+    sqlite3_stmt *stmt;
+    int rc = sqlite3_prepare_v2(db, cmd.c_str(), -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        std::cerr << "error: " << sqlite3_errmsg(db);
+        return ret;
+    }
+    
+    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        ret.push_back(std::string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0))));
+    }
+    if (rc != SQLITE_DONE) {
+        std::cerr << "error: " << sqlite3_errmsg(db);
+    }
+
+    sqlite3_finalize(stmt);
 
     sqlite3_close(db);
     return ret;
